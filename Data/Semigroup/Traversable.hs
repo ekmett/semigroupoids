da93fd53300565f425564791d8764d9594e62169
Edward Kmett <ekmett@gmail.com>
2011-01-09 19:27:51 -0500

    added mutual definitions

--- Data/Semigroup/Traversable.hs
+++ Data/Semigroup/Traversable.hs
@@ -19,35 +19,13 @@ import Data.Functor.Apply
 import Data.Semigroup.Foldable
 import Data.Traversable
 import Data.Semigroup
--- import Data.Functor.Identity
 
 class (Foldable1 t, Traversable t) => Traversable1 t where
   traverse1 :: FunctorApply f => (a -> f b) -> t a -> f (t b)
   sequence1 :: FunctorApply f => t (f b) -> f (t b)
 
+  sequence1 = traverse1 id
+  traverse1 f = sequence1 . fmap f
+
 foldMap1Default :: (Traversable1 f, Semigroup m) => (a -> m) -> f a -> m
 foldMap1Default f = getConst . traverse1 (Const . f)
-
-{-
-instance Traversable1 Identity where
-  traverse1 f (Identity a) = Identity <$> f a
--}
-
-
-{-
-instance Traversable ((,)a) where traverse = traverse1
-instance Traversable1 ((,)a) where 
-  traverse1 f (a,b) = (,) a <$> f b
-
-instance Traversable ((,,) a b) where traverse = traverse1
-instance Traversable1 ((,) a b) where 
-  traverse1 f (a,b,c) = (,) a b <$> f c
-
-instance Traversable ((,,) a b c) where traverse = traverse1
-instance Traversable1 ((,,) a b c) where 
-  traverse1 f (a,b,c,d) = (,,) a b c <$> f d
-
-instance Traversable ((,,,) a b c d) where traverse = traverse1
-instance Traversable1 ((,,,) a b c d) where 
-  traverse1 f (a,b,c,d,e) = (,,,) a b c d <$> f e
--}
