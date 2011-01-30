9616a2ae9535edfbe256935c8ded426e643bc26e
Edward Kmett <ekmett@gmail.com>
2011-01-30 06:11:15 -0500

    Op is a semigroupoid

--- Data/Semigroupoid.hs
+++ Data/Semigroupoid.hs
@@ -1,8 +1,22 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Semigroupoid
+-- Copyright   :  (C) 2007-2011 Edward Kmett
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  Edward Kmett <ekmett@gmail.com>
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- A semigroupoid satisfies all of the requirements to be a Category except 
+-- for the existence of identity arrows.
+----------------------------------------------------------------------------
 module Data.Semigroupoid (Semigroupoid(..)) where
 
 import Control.Arrow
 import Data.Functor.Bind
 import Control.Comonad
+import Data.Functor.Contravariant
 
 -- | 'Control.Category.Category' sans 'Control.Category.id'
 class Semigroupoid c where
@@ -16,3 +30,6 @@ instance Bind m => Semigroupoid (Kleisli m) where
 
 instance Extend w => Semigroupoid (Cokleisli w) where
   Cokleisli f `o` Cokleisli g = Cokleisli $ f . extend g
+
+instance Semigroupoid Op where
+  Op f `o` Op g = Op (g `o` f)
