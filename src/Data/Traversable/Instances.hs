688a6af5113014bbe9b9285f46e82d2437919b3c
Edward Kmett <ekmett@gmail.com>
2014-07-30 19:56:43 -0400

    properly guard in CPP

--- src/Data/Traversable/Instances.hs
+++ src/Data/Traversable/Instances.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE CPP #-}
+#ifndef MIN_VERSION_transformers
+#define MIN_VERSION_transformers(x,y,z) 1
+#endif
+#ifndef MIN_VERSION_base
+#define MIN_VERSION_base(x,y,z) 1
+#endif
 -- | Placeholders for missing instances of Traversable, until base catches up and adds them
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.Traversable.Instances where
 
 #if !(MIN_VERSION_transformers(0,3,0))
 import Control.Monad.Trans.Identity
+#endif
+
+#if !((MIN_VERSION_transformers(0,3,0)) && (MIN_VERSION_base(4,7,0)))
 import Data.Foldable
 import Data.Traversable
+#endif
 
+#if !(MIN_VERSION_base(4,7,0))
+import Data.Monoid
+#endif
+
+#if !(MIN_VERSION_transformers(0,3,0))
 instance Foldable m => Foldable (IdentityT m) where
   foldMap f = foldMap f . runIdentityT
 
