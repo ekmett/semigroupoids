88b962a419768515d73e62e5438959ebc9184532
Edward Kmett <ekmett@gmail.com>
2014-07-30 19:54:02 -0400

    moved all the orphan instances over from lens

--- src/Data/Traversable/Instances.hs
+++ src/Data/Traversable/Instances.hs
@@ -14,3 +14,27 @@ instance Foldable m => Foldable (IdentityT m) where
 instance Traversable m => Traversable (IdentityT m) where
   traverse f = fmap IdentityT . traverse f . runIdentityT
 #endif
+
+--------------------------------------
+
+#if !(MIN_VERSION_base(4,7,0))
+instance Foldable ((,) b) where
+  foldMap f (_, a) = f a
+
+instance Traversable ((,) b) where
+  traverse f (b, a) = (,) b <$> f a
+
+instance Foldable (Either a) where
+  foldMap _ (Left _) = mempty
+  foldMap f (Right a) = f a
+
+instance Traversable (Either a) where
+  traverse _ (Left b) = pure (Left b)
+  traverse f (Right a) = Right <$> f a
+
+instance Foldable (Const m) where
+  foldMap _ _ = mempty
+
+instance Traversable (Const m) where
+  traverse _ (Const m) = pure $ Const m
+#endif
