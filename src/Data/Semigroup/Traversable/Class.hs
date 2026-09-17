bb01e0781908a93c84a7b373c8fb38006f91239d
Julian Brunner <julian.brunner@gmail.com>
2026-09-17 14:22:53 +0200

    improve performance of `traverse1` in `Traversable1 NonEmpty` instance

--- src/Data/Semigroup/Traversable/Class.hs
+++ src/Data/Semigroup/Traversable/Class.hs
@@ -35,11 +35,12 @@ import Data.Complex
 import Data.Functor.Identity
 import Data.Functor.Product as Functor
 import Data.Functor.Sum as Functor
+import Data.Foldable1
 import Data.List.NonEmpty (NonEmpty(..))
+import qualified Data.List.NonEmpty as N
 import qualified Data.Monoid as Monoid
 import Data.Orphans ()
 import Data.Semigroup as Semigroup
-import Data.Semigroup.Foldable
 import Data.Semigroup.Bifoldable
 #ifdef MIN_VERSION_tagged
 import Data.Tagged
@@ -209,7 +210,8 @@ instance Traversable1 Tree where
 #endif
 
 instance Traversable1 NonEmpty where
-  traverse1 f (a :| as) = foldr (\b g x -> (\a' (b':| bs') -> a' :| b': bs') <$> f x <.> g b) (fmap (:|[]) . f) as a
+  traverse1 f = foldrMap1 (fmap (:| []) . f) (liftF2 (\ x xs -> x :| N.toList xs) . f)
+  {-# INLINE traverse1 #-}
 
 instance Traversable1 ((,) a) where
   traverse1 f (a, b) = (,) a <$> f b
